e62866d7da11215f23648c39cd52ebcd09dc28c3
quanzz123 <nguyenhongquan8a25062003@gmail.com>
2023-03-18 08:39:47 +0700

    add2

--- mang2-new.cpp
+++ mang2-new.cpp
@@ -33,6 +33,20 @@ void in_mang(float **arr, int n, int m)
         cout << endl;
     }
 }
+void sum_row(float **arr, int n, int m)
+{
+    int sum_row[n];
+    for(int i=0; i<n; i++)
+    {
+        int sum = 0 ;
+        for(int j=0; j<m; j++)
+        {
+            sum += arr[i][j];
+        }
+        sum_row[i] = sum;
+        cout << "\ntong cua hang " << i+1 << ":  " << sum << endl;
+    }
+}
 int main()
 {
     int n,m;
@@ -47,8 +61,9 @@ int main()
     nhap_mang(arr,n,m);
 
     in_mang(arr,n,m);
-    
-    in_mang(arr,n,m);
+
+    cout << "\ntong tung hang: ";
+    sum_row(arr,n,m);
     
     for(int i=0; i<n; i++)
     delete arr[i];
