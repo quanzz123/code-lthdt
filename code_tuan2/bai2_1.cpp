ca25d35c18347c22bc391e76950cbcc2a3661dc9
quanzz123 <nguyenhongquan8a25062003@gmail.com>
2023-03-23 17:42:40 +0700

    add-sub

---
+++ code_tuan2/bai2_1.cpp
@@ -0,0 +1,71 @@
+#include"iostream"
+
+using namespace std;
+
+void taoVecto( float arr[], int n){
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+}
+void inVecto(float arr[], int n){
+    cout << "(";
+    for(int i=0; i<n; i++){
+        cout <<arr[i] << " ";
+    }
+    cout << ")";
+    cout << endl;
+}
+void tinh(float arr[], int n, int m){
+    if(n == m){
+        
+    } else{
+        cout << "\nhai vecto khong cung chieu";
+
+    }
+}
+void vector_add_subtract(float* a, int n, float* b, int m)
+{
+    if(n!=m)
+    {
+        cout<<"Hai vector khong cung chieu!"<<endl;
+        return;
+    }
+    float* sum = new float[n];
+    float* diff = new float[n];
+    for(int i=0;i<n;i++)
+    {
+        sum[i] = a[i]+b[i];
+        diff[i] = a[i]-b[i];
+    }
+    cout<<"Tong cua hai vector la: ";
+    for(int i=0;i<n;i++)
+        cout<<sum[i]<<" ";
+    cout<<endl<<"Hieu cua hai vector la: ";
+    for(int i=0;i<n;i++)
+        cout<<diff[i]<<" ";
+    cout<<endl;
+    delete[] sum;
+    delete[] diff;
+}
+
+int main(){
+    int m,n;
+    
+    cout << "\nnhap so phan tua cua vecto thu nhat: ";
+    cin >> n;
+    cout << "\nnhap so phan tu cua vecto thu hai: ";
+    cin >> m;
+    float *arr = new float[n];
+    float *arr2 = new float[m];
+    taoVecto(arr, n);
+    taoVecto(arr2 , m);
+    inVecto(arr,n);
+    inVecto(arr2,m);
+
+    vector_add_subtract(arr,n,arr2,m);
+
+    delete[] arr;
+    delete[] arr2;
+    
+}
+
