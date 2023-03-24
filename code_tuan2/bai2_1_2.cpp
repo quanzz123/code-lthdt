cdf79d18664d8a2e3ecf354febc1aed986719056
quanzz123 <nguyenhongquan8a25062003@gmail.com>
2023-03-24 15:27:49 +0700

    ham

---
+++ code_tuan2/bai2_1_2.cpp
@@ -0,0 +1,69 @@
+#include"iostream"
+using namespace std;
+ void taovecto(float *&a, int n){
+    float *a = new float[n];
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+ }
+ void invecto(float *a, int n){
+    cout << "(";
+    for(int i=0; i<n; i++){
+        cout << a[i];
+    }
+    cout << ")";
+    cout << endl;
+ }
+ void giai_phong(float *a){
+    delete a;
+ }
+ void add(float *a, int n, float *b,int m, float * &tong){
+    if(n != m){
+        cout << "\nhai vecto khong cung chieu!" << endl;
+        return;
+    }
+    tong = new float[n];
+    for(int i=0; i<n; i++){
+        tong[i] = a[i] + b[i];
+    }
+ }
+ void sub(float *a, int n, float *b, int m, float *&hieu){
+    if(n != m){
+        cout << "\nhai vecto khong cung chieu!" << endl;
+        return;
+    }
+    hieu = new float[n];
+    for(int i=0; i<n; i++){
+        hieu[i] = a[i] - b[i];
+    }
+ }
+ int main(){
+    int n,m;
+    cout << "\nnhap so phan tu cua vecto thu nhat: ";
+    cin >> n;
+    cout << "\nnhap so phan tu cua vecto thu hai ";
+    cin >> m;
+    float *a, *b, *tong, *hieu;
+    taovecto(a,n);
+    cout << endl;
+    taovecto(b,m);
+
+    invecto(a,n);
+    cout << endl;
+    invecto(b,m);
+
+    add(a,n,b,m,tong);
+    sub(a,n,b,m,hieu);
+
+    cout << "\ntong cua hai vecto: " << endl;
+    invecto(tong,n);
+
+    cout << "\nhieu cua hai vecto: " << endl;
+    invecto(hieu,n);
+    giai_phong(a);
+    giai_phong(b);
+    giai_phong(tong);
+    giai_phong(hieu);
+
+
+ }
--- code_tuan2/bai2_2.cpp
+++ code_tuan2/bai2_2.cpp
@@ -1,84 +1,78 @@
 #include"iostream"
-
 using namespace std;
 
-void tao_mang(int **&arr, int n, int m){
+void nhap_mang(float** &a, int n, int m ){
+    a = new float*[n];
     for(int i=0; i<n; i++){
-        arr[i] = new int[m];
-    }
-
-}
-void nhap_mang(int **arr, int n, int m){
-    for(int i=0; i<n; i++){
+        a[i] = new float[m];
         for(int j=0; j<m; j++){
-            cout << "\nnhap gia tri phan tu thu [" << i << "][" << j << "]";
-            cin >> arr[i][j];
+            cout << "nhap phan tu thu [" << i << "][" << j << "]";
+            cin >> a[i][j];
         }
     }
 }
-
-void in_mang(int **arr, int n, int m){
+void in_mang(float** a, int n, int m){
     for(int i=0; i<n; i++){
         for(int j=0; j<m; j++){
-            cout << arr[i][j] << " ";
-
+            cout << a[i][j] << " ";
         }
         cout << endl;
     }
-
 }
-void tong_hieu(int **arr1, int n, int m, int **arr2, int p, int q){
-    if(n == p && m == q){
-        int **tong = new int*[n];
-        int **hieu = new int *[n];
-        for(int i=0; i<n; i++){
-            tong[i] = new int[m];
-            hieu[i]= new int[m];
-            for(int j=0; j<m; j++){
-                tong[i][j] = arr1[i][j] + arr2[i][j];
-                hieu[i][j] = arr2[i][j] - arr2[i][j];
-            }
-        
+void add(float** a, float** b, float** &tong, int n, int m){
+    tong = new float*[n];
+    for(int i=0; i<n; i++){
+        tong[i] = new float[m];
+        for(int j=0; j<m; j++){
+            tong[i][j] = a[i][j] + b[i][j];
 
         }
     }
-    else(
-        cout << "\nhai ma tran khong cug cap!";
-    )
-
-}    
-void hieu(int **arr1, int n, int m, int **arr2, int p, int q){
-    if(n == p && m == q){
-        int **hieu = new int*[n];
-        for(int i=0; i<n; i++){
-            hieu = new int[m];
-            for(int j=0; j<m; j++){
-                hie
-            }
+}
+void sub(float** a, float** b, float** &hieu, int n, int m){
+    hieu = new float*[n];
+    for(int i=0; i<n; i++){
+        hieu[i] = new float[m];
+        for(int j=0; j<m; j++){
+            hieu[i][j] = a[i][j] - b[i][j];
         }
     }
+}
+void giai_phong(float** a, int n ){
+    for(int i=0; i<n; i++){
+        delete a[i];
+    }
+    delete a;
 }
 int main(){
     int n,m,p,q;
-    cout << "\nnhap so hang cua ma tran thu nhat: ";
-    cin >> n;
-    cout << "\nnhap so cot cua ma tran thu nhat: ";
-    cin >> m;
-    cout << "\nnhap so hang cua ma tran thu hai: ";
-    cin >> p;
-    cout << "\nnhap so cot cua ma trna thu hai: ";
-    cin >> q;
-    int  **arr1;
-    arr1 = new float*[n];
-    int **arr2;
-    arr2 = new int[p];
-    for(int i=0; i<n; i++)
-    delete arr1[i];
-    delete arr1;
-    for(int i=0; i<p; i++)
-    delete arr2[i];
-    delete arr2;
-    return 0;
+    cout << "nhap so hang so cot cua ma tran thu nhat: ";
+    cin >> n >> m;
+    cout << "nhap so hang so cot cua ma tran thu hai: ";
+    cin >> p >> q;
+    float** a, **b, **tong, **hieu;
+    if(n == p && m == q){
+        nhap_mang(a,n,m);
+        cout << endl;
+        nhap_mang(b,p,q);
 
+        in_mang(a,n,m);
+        cout << endl;
+        in_mang(b,p,q);
+
+        add(a,b,tong,n,m);
+        sub(a,b,hieu,n,m);
+
+        cout << "\ntong cua hai ma tran: " << endl;
+        in_mang(tong,n,m);
+
+        cout << "\nhieu cua hai ma tran : " << endl;
+        in_mang(hieu,n,m);
+
+        giai_phong(a,n);
+        giai_phong(b,p);
+        giai_phong(tong,n);
+        giai_phong(hieu,n);
+    }
 
 }
