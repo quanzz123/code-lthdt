610b9da3c87656e1314ac60aa3d5ca620328f1bf
quanzz123 <nguyenhongquan8a25062003@gmail.com>
2023-04-13 14:46:42 +0700

    main

--- bài tập tự luyện/cdcanhac.cpp
+++ bài tập tự luyện/cdcanhac.cpp
@@ -2,3 +2,84 @@
 Tên đĩa, số lượng bài, giátiền và các phương thức nhập, xuất dữ liệu.
 Viết chương trình chính thực hiện nhập vào một danh sách n đĩa ca nhạc,sau đó hiển thị danh sách vừa nhập. 
 Tìm và hiển thị thông tin của đĩa có sốlượng bài hát lớn nhất*/
+
+#include<bits/stdc++.h>
+using namespace std;
+class CDCANHAC {
+    private:
+        string TenCD;
+        int SoLuong;
+        int GiaTien;
+    public:
+        CDCANHAC(string TenCD = "", int SoLuong = 0, int GiaTien = 0) {
+            this->TenCD = TenCD;
+            this->SoLuong = SoLuong;
+            this->GiaTien = GiaTien;
+        }
+        void Nhap_data() {
+            cout << "Nhap ten dia CD: ";
+            getline(cin, TenCD);
+            cout << "Nhap so luong dia cd: ";
+            cin >> SoLuong;
+            cout << "Nhap gia tien cua dia cd: ";
+            cin >> GiaTien;
+            cin.ignore();
+        }
+        int get_SoLuong() {
+            return SoLuong;
+        }
+        void In_data() {
+            cout << setw(20) << left << TenCD;
+            cout << setw(10) << right << SoLuong;
+            cout << setw(15) << right << GiaTien;
+            cout << setw(15) << right << SoLuong*GiaTien << endl;
+            cout << setfill('-') << setw(60) << "-" << endl;
+            cout << setfill(' ');
+        }
+        ~CDCANHAC() {
+            cout << "\nHuy bo doi tuong cd ca nhac!" << endl;
+        }
+};
+int main() {
+    int n;
+    cout << "Nhap so luong dia cd: ";
+    cin >> n;
+    cin.ignore();
+
+    CDCANHAC* cd = new CDCANHAC[n];
+
+    for(int i=0 ; i<n; i++) {
+        cout << "\nNhap thong tin cd thu " << i+1 << ": " << endl;
+        cd[i].Nhap_data();
+        cout << endl;
+    }
+    cout << "\n\n\t\t===DANH SACH THONG TIN CAC LOAI DIA CD===\n\n";
+    cout << setw(20) << left << "TenCD";
+            cout << setw(10) << right << "SoLuong";
+            cout << setw(15) << right << "GiaTien";
+            cout << setw(15) << right << "Thanh Tien" << endl;
+            cout << setfill('-') << setw(60) << "-" << endl;
+            cout << setfill(' ');
+    for(int i=0; i<n; i++) {
+        cd[i].In_data();
+    }
+    cout << "\n\n\t\t=== THONG TIN DIA CD CO SO LUONG LON NHAT===\n\n";
+    cout << setw(20) << left << "TenCD";
+            cout << setw(10) << right << "SoLuong";
+            cout << setw(15) << right <<" GiaTien";
+            cout << setw(15) << right << "Thanh Tien" << endl;
+            cout << setfill('-') << setw(60) << "-" << endl;
+            cout << setfill(' ');
+    int max = 0;
+    for(int i=0; i<n; i++) {
+        if(cd[i].get_SoLuong() > max) {
+            max = cd[i].get_SoLuong();
+        }
+    }
+    for(int i=0; i<n; i++) {
+        if(cd[i].get_SoLuong() == max) {
+            cd[i].In_data();
+        }
+    }
+    delete[] cd;
+}
--- bài tập tự luyện/thisinh.cpp
+++ bài tập tự luyện/thisinh.cpp
@@ -7,4 +7,88 @@
 - In thông tin số báo danh, tên, điểm và tổng điểm thi 3 môn
 Trên cơ sở lớp đã xây dựng được,viêt chương trình làm các công việc sau
 - Nhập danh sách kết quả thi của các thí sinh vào từ bàn phím
-- Dưa ra màn hình danh sách thí sinh trúng tuyển (cho phép nhập điểmchuẩn)
+- Dưa ra màn hình danh sách thí sinh trúng tuyển (cho phép nhập điểmchuẩn)*/
+
+#include<bits/stdc++.h>
+using namespace std;
+class ThiSinh {
+    private:
+        string SBD;
+        string Ten;
+        float M1, M2, M3;
+    public:
+        ThiSinh(string SBD="" , string Ten = "", float M1 = 0, float M2 = 0, float M3 = 0) {
+            this->SBD = SBD;
+            this->Ten = Ten;
+            this->M1 = M1;
+            this->M2 = M2;
+            this->M3 = M3;
+        }
+        void Nhapthongtin() {
+            cout << "Nhap so bao danh thi sinh: ";
+            getline(cin, SBD);
+            cout << "Nhap ho ten thi sinh: ";
+            getline(cin, Ten);
+            cout << "Nhap diem mon 1: ";
+            cin >> M1;
+            cout << "Nhap diem mon 2: ";
+            cin >> M2;
+            cout << "Nhap diem mon 3: ";
+            cin >> M3;
+            cin.ignore();
+        }
+        float sum() {
+            return M1 + M2 + M3;
+        }
+        void inthongtin() {
+            cout << setw(20) << left << SBD;
+            cout << setw(10) << right << Ten;
+            cout << setw(15) << right << M1;
+            cout << setw(15) << right << M2;
+            cout << setw(15) << right << M3 << endl;
+            cout << setfill('-') << setw(75) << "-" << endl;
+            cout << setfill(' ');
+        }
+        ~ThiSinh() {
+            cout << "\nHuy bo doi tuong ThiSinh" << endl;
+        }
+
+};
+int main() {
+    int n;
+    cout << "\nNhap so luong thi sinh: ";
+    cin >> n;
+    cin.ignore();
+
+    ThiSinh* ds = new ThiSinh[n];
+
+    for(int i=0; i<n; i++) {
+        cout << "\nNhap thong tin thi sinh thu " << i+1 << ": " << endl;
+        ds[i].Nhapthongtin();
+    }
+    cout << "\n\n\t\t===DANH SANH THI SINH===\n\n";
+    cout << setw(20) << left << "SBD";
+            cout << setw(10) << right << "Ho va Ten";
+            cout << setw(15) << right << "Diem M1";
+            cout << setw(15) << right << "Diem M2";
+            cout << setw(15) << right << "Diem M3" << endl;
+            cout << setfill('-') << setw(75) << "-" << endl;
+            cout << setfill(' ');
+    for(int i=0; i<n; i++) {
+        ds[i].inthongtin();
+    }
+    cout << "\n\n\t\t===DANH SACH THI SINH TRUNG TUUYEN===\n\n";
+     cout << setw(20) << left << "SBD";
+            cout << setw(10) << right << "Ho va Ten";
+            cout << setw(15) << right << "Diem M1";
+            cout << setw(15) << right << "Diem M2";
+            cout << setw(15) << right << "Diem M3" << endl;
+            cout << setfill('-') << setw(75) << "-" << endl;
+            cout << setfill(' ');
+    for(int i=0; i<n; i++) {
+        if(ds[i].sum() >= 24.0) {
+            ds[i].inthongtin();
+        }
+    }
+    delete[] ds;
+}
