3be9051bda0c5c0db383def90ca30587e2c31099
quanzz123 <nguyenhongquan8a25062003@gmail.com>
2023-04-19 14:46:44 +0700

    main

---
+++ code_tuan6/libary6_3.h
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Matrix {
+    private:
+        int m,n;
+        int **v;
+    public:
+        Matrix() {
+           n = 0;
+           m = 0;
+           v = NULL; 
+        }
+        Matrix(int m, int n);
+        Matrix(int m, int n, int val);
+        void add();
+        void print();
+        ~Matrix();
+        Matrix(Matrix &a);
+        void operator=(Matrix &a);
+        friend istream& operator>>(istream& is, Matrix& mat);
+        //friend ostream& operator<<(ostream& os, Matrix& mat);
+};
+// https://github.com/bogdanmacovei/OOP-Matrix-Class
+//https://github.com/Agil1ty/Matrix
