cb2919f9fe9430cc414e020bd66be6766547bf9f
quanzz123 <nguyenhongquan8a25062003@gmail.com>
2023-04-29 23:06:02 +0700

    main

---
+++ code_tuan7/Bai7_2.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+class MyDate {
+    private:
+        int date, month, year;
+    public:
+        MyDate(int d =0, int m = 0, int y= 0) {
+            date = d;
+            month = m;
+            year = y;
+        }
+        void add() {
+            cout << "Nhap ngay: ";
+            cin >> date;
+            cout << "Nhap thang: ";
+            cin >> month;
+            cout << "Nhap nam: ";
+            cin >> year;
+        }
+        void print() {
+            //cout << setfill('-') << setw(55) << "-" << endl;
+            //cout << setfill(' ');
+            cout << "|" << setw(15) << right << date << setw(3);
+            cout << "|" << setw(10) << right << month << setw(3);
+            cout << "|" <<setw(10) << right << year << setw(3);
+            cout << "|" << setw(9) << right << date << "/" << month << "/" << year << setw(5) << "|" << endl;
+            cout << setfill('_') << setw(73) << "_" << endl;
+            cout << setfill(' ');
+        }
+        friend 
+
+};
+int main() {
+    int n;
+    cout << "Nhap n = ";
+    cin >> n;
+    MyDate *list = new MyDate[n];
+    for(int i=0; i<n; i++) {
+        list[i].add(); cout << endl;
+    }
+    cout << setw(3) << setfill('_') << setw(73) << "_" <<endl;
+    cout << setfill(' ');
+    cout << "|" << setw(5) << right << "stt" << setw(3);
+    cout << "|" << setw(15) << right << "date" << setw(3);
+    cout << "|" << setw(10) << right << "month" << setw(3);
+    cout << "|" << setw(10) << right << "year" << setw(3);
+    cout << "|" << setw(15) << right << "day" << setw(5) << "|" << endl;
+    cout << setfill('_') << setw(73) << "_" <<endl;
+    cout << setfill(' ');
+    for(int i=0; i<n; i++) {
+        cout << "|" << setw(5) << right << i+1 << setw(3);
+        list[i].print();
+        cout << endl;
+    }
+    return 0;
+
+}
