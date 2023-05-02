f4deaf811638f8eb2901a40ee2dd354ae7458c98
quanzz123 <nguyenhongquan8a25062003@gmail.com>
2023-05-02 20:19:33 +0700

    main

--- code_tuan7/test7_3.cpp
+++ code_tuan7/test7_3.cpp
@@ -19,8 +19,42 @@ class MyTime {
             cout << endl;
         }
         void display() {
-            cout << "|" << "\t" << setw(5) << left << hours;
-            cout << "|" << "\t" << setw(5) << left << minutes;
-            cout << "|" << "\t" << setw(5) << left << seconds;
+            cout << "|" << "\t" << setw(5) << left << hours << "\t";
+            cout << "|" << "\t" << setw(5) << left << minutes << "\t";
+            cout << "|" << "\t" << setw(5) << left << seconds << "\t";
+            cout << "|" << "\t" << setw(5) << right << hours << ":" << minutes << ":" << seconds << "\t" << "|" << endl;
+
         }
+        friend bool operator>=(MyTime& time1, MyTime& time2);
 };
+bool operator>=(MyTime& time1, MyTime& time2) {
+    if(time1.hours > time2.hours) {
+        return true;
+    } else if(time1.hours == time2.hours && time1.minutes > time2.minutes) {
+        return true;
+    } else if(time1.minutes == time2.minutes && time1.seconds > time2.seconds) {
+        return true;
+    } else {
+        return false;
+    }
+}
+int main() {
+    int n;
+    cout << "Nhap n= ";
+    cin >> n;
+    MyTime *time = new MyTime[n];
+    for(int i=0; i<n; i++) {
+        cout << "Nhap time thu: " << i+1 << endl;
+        time[i].add();
+    }
+    cout << "|" << "\t" << setw(5) << left << "stt" << "\t";
+    cout << "|" << "\t" << setw(5) << left << "hours" << "\t";
+    cout << "|" << "\t" << setw(5) << left << "minutes" << "\t";
+    cout << "|" << "\t" << setw(5) << left << "seconds" << "\t";
+    cout << "|" << "\t" << setw(10) << right << "time" << "\t" << "|" << endl;
+    for(int i=0; i<n;i++) {
+        cout << "|" << "\t" << setw(5) << left << i+1 << "\t";
+        time[i].display();
+    }
+
+}
