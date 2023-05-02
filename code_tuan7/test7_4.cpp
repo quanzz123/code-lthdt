7653df6b6817c99df17b2d01e3929d20bf0b044b
quanzz123 <nguyenhongquan8a25062003@gmail.com>
2023-05-02 22:19:06 +0700

    main

---
+++ code_tuan7/test7_4.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Frac {
+    private:
+        int numerator, denominator;
+    public:
+        Frac(int n=0, int d=0) {
+            numerator = n;
+            denominator = d;
+        }
+        
+};
