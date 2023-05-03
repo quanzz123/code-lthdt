305d06ecca414768028ce2e7a844ae665a5e326a
quanzz123 <nguyenhongquan8a25062003@gmail.com>
2023-05-03 15:53:17 +0700

    main

--- code_tuan7/test7_4.cpp
+++ code_tuan7/test7_4.cpp
@@ -2,11 +2,45 @@
 using namespace std;
 class Frac {
     private:
-        int numerator, denominator;
+        ///int numerator: tử số, denominator = mẫu số;
+        int num, den;
     public:
         Frac(int n=0, int d=0) {
-            numerator = n;
-            denominator = d;
+            num = n;
+            den = d;
         }
-        
+         friend istream& operator>>(istream& is, Frac& f);
+            
+        friend ostream& operator<<(ostream& os, Frac& f);
+        bool operator >= (Frac other) {
+        return (num * other.den >= other.num * den);
+    }
 };
+istream& operator>>(istream& is, Frac& f) {
+    cout << "numerator: ";
+    is >> f.num;
+    do {
+    cout << "denominator: ";
+    is >> f.den;
+        if(f.den == 0) {
+            cout << "the denominator must be no-zero! please re-enter" << endl;
+
+        } while(f.den == 0);
+        return is;
+    }
+}
+ostream& operator<<(ostream& os, Frac& f) {
+    os << f.num << "/" << f.den;
+    return os;
+}
+int main() {
+    Frac arr[3];
+    cout << "Enter 3 fraction: " << endl;
+    for(int i=0; i<3; i++) {
+        cin >> arr[i];
+    }
+    cout << "output: " << endl;
+    for(int i=0; i<3; i++) {
+        cout << arr[i] << " ";
+    }
+}
