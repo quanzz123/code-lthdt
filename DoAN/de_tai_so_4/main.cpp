97c9ca957d352fff2f70b778de7e2ff92f28fbc0
quanzz123 <nguyennquandc37@gmail.com>
2023-06-10 10:34:24 +0700

    main

--- DoAN/de_tai_so_4/main.cpp
+++ DoAN/de_tai_so_4/main.cpp
@@ -14,17 +14,20 @@ protected:
     string tacGia;
     string nhaXuatBan;
     string ngayXuatBan;
+    string phuongthuc;
     int soTrang;
     int soBanLuu;
 
 public:
-    Sach(string maSo, string ten, string chuDe, string tacGia, string nhaXuatBan, string ngayXuatBan, int soTrang, int soBanLuu) {
+    Sach(string maSo, string ten, string chuDe, string tacGia, string nhaXuatBan, string ngayXuatBan, string phuongthuc, int soTrang, int soBanLuu) {
         maSoSach = maSo;
         tenSach = ten;
         this->chuDe = chuDe;
         this->tacGia = tacGia;
         this->nhaXuatBan = nhaXuatBan;
         this->ngayXuatBan = ngayXuatBan;
+        this->phuongthuc = phuongthuc;
+
         this->soTrang = soTrang;
         this->soBanLuu = soBanLuu;
     }
@@ -36,6 +39,7 @@ public:
         cout << "Tac gia: " << tacGia << endl;
         cout << "nha xuat ban: " << nhaXuatBan << endl;
         cout << "ngay xuat ban: " << ngayXuatBan << endl;
+        cout << "phuong thuc: " << phuongthuc << endl;
         cout << "So trang: " << soTrang << endl;
         cout << "So ban luu thu vien: " << soBanLuu << endl;
     }
@@ -88,6 +92,12 @@ public:
     void setNgayXuatBan(string ngayXuatBan) {
         this->ngayXuatBan = ngayXuatBan;
     }
+    string getphuongthuc() {
+        return phuongthuc;
+    }
+    void setphuongthuc(string phuongthuc) {
+        this->phuongthuc = phuongthuc;
+    }
 
     int getSoTrang() {
         return soTrang;
@@ -113,8 +123,8 @@ private:
     string ngayHenTra;
 
 public:
-    SachMuonVe(string maSo, string ten, string chuDe, string tacGia, string nhaXuatBan, string ngayXuatBan, int soTrang, int soBanLuu, string ngayMuon, string ngayHenTra)
-        : Sach(maSo, ten, chuDe, tacGia, nhaXuatBan, ngayXuatBan, soTrang, soBanLuu) {
+    SachMuonVe(string maSo, string ten, string chuDe, string tacGia, string nhaXuatBan, string ngayXuatBan, string phuongthuc, int soTrang, int soBanLuu, string ngayMuon, string ngayHenTra)
+        : Sach(maSo, ten, chuDe, tacGia, nhaXuatBan, ngayXuatBan, phuongthuc, soTrang, soBanLuu) {
         this->ngayMuon = ngayMuon;
         this->ngayHenTra = ngayHenTra;
     }
@@ -138,8 +148,8 @@ private:
     string gioTra;
 
 public:
-    SachMuonDoc(string maSo, string ten, string chuDe, string tacGia, string nhaXuatBan, string ngayXuatBan, int soTrang, int soBanLuu, string gioMuon, string gioTra)
-        : Sach(maSo, ten, chuDe, tacGia, nhaXuatBan, ngayXuatBan, soTrang, soBanLuu) {
+    SachMuonDoc(string maSo, string ten, string chuDe, string tacGia, string nhaXuatBan, string ngayXuatBan, string phuongthuc, int soTrang, int soBanLuu, string gioMuon, string gioTra)
+        : Sach(maSo, ten, chuDe, tacGia, nhaXuatBan, ngayXuatBan, phuongthuc, soTrang, soBanLuu) {
         this->gioMuon = gioMuon;
         this->gioTra = gioTra;
     }
@@ -169,31 +179,42 @@ int main() {
             // ... (phân tách dữ liệu và tạo đối tượng SachMuonVe) ...
             // Sau đó, thêm đối tượng SachMuonVe vào danh sách danhSachSach
             stringstream ss(line);
-        string maSo, ten, chuDe, tacGia, nhaXuatBan, ngayXuatBan, ngayMuon, ngayHenTra, gioMuon, gioTra;
+        string maSo, ten, chuDe, tacGia, nhaXuatBan, ngayXuatBan, phuongthuc, ngayMuon, ngayHenTra, gioMuon, gioTra;
         int soTrang, soBanLuu;
+        
         getline(ss, maSo, ',');
+       
         getline(ss, ten, ',');
+        
         getline(ss, chuDe, ',');
+        
         getline(ss, tacGia, ',');
+       
         getline(ss, nhaXuatBan, ',');
+       
         getline(ss, ngayXuatBan, ',');
+        getline(ss, phuongthuc,',');
+
+       
         ss >> soTrang;
-        ss.ignore();
+       
         ss >> soBanLuu;
-        ss.ignore();
+       
         getline(ss, ngayMuon, ',');
         getline(ss, ngayHenTra, ',');
         getline(ss, gioMuon, ',');
         getline(ss, gioTra, ',');
 
-        if (!gioMuon.empty() && !gioTra.empty()) {
-            SachMuonDoc* sachMuonDoc = new SachMuonDoc(maSo, ten, chuDe, tacGia, nhaXuatBan, ngayXuatBan, soTrang, soBanLuu, gioMuon, gioTra);
+        if (!gioMuon.empty() && !gioTra.empty() && phuongthuc == "doc") {
+            SachMuonDoc* sachMuonDoc = new SachMuonDoc(maSo, ten, chuDe, tacGia, nhaXuatBan, ngayXuatBan, phuongthuc, soTrang, soBanLuu, gioMuon, gioTra);
+             
             danhSachSach.push_back(sachMuonDoc);
-        } else if (!ngayMuon.empty() && !ngayHenTra.empty()) {
-            SachMuonVe* sachMuonVe = new SachMuonVe(maSo, ten, chuDe, tacGia, nhaXuatBan, ngayXuatBan, soTrang, soBanLuu, ngayMuon, ngayHenTra);
+        } else if (!ngayMuon.empty() && !ngayHenTra.empty() && phuongthuc == "muon") {
+            SachMuonVe* sachMuonVe = new SachMuonVe(maSo, ten, chuDe, tacGia, nhaXuatBan, ngayXuatBan, phuongthuc, soTrang, soBanLuu, ngayMuon, ngayHenTra);
+            
             danhSachSach.push_back(sachMuonVe);
         } else {
-            Sach* sach = new Sach(maSo, ten, chuDe, tacGia, nhaXuatBan, ngayXuatBan, soTrang, soBanLuu);
+            Sach* sach = new Sach(maSo, ten, chuDe, tacGia, nhaXuatBan, ngayXuatBan, phuongthuc, soTrang, soBanLuu);
             danhSachSach.push_back(sach);
         }
     }
@@ -203,46 +224,24 @@ int main() {
         cout << "Khong the mo file danh_sach_sach.txt" << endl;
         return 0;
     }
-    vector<SachMuonVe*> danhSachSachMuonVe;
-vector<SachMuonDoc*> danhSachSachMuonDoc;
-
-// Thêm sách vào danh sách tương ứng
-if (!gioMuon.empty() && !gioTra.empty()) {
-    SachMuonDoc* sachMuonDoc = new SachMuonDoc(maSo, ten, chuDe, tacGia, nhaXuatBan, ngayXuatBan, soTrang, soBanLuu, gioMuon, gioTra);
-    danhSachSachMuonDoc.push_back(sachMuonDoc);
-} else if (!ngayMuon.empty() && !ngayHenTra.empty()) {
-    SachMuonVe* sachMuonVe = new SachMuonVe(maSo, ten, chuDe, tacGia, nhaXuatBan, ngayXuatBan, soTrang, soBanLuu, ngayMuon, ngayHenTra);
-    danhSachSachMuonVe.push_back(sachMuonVe);
-} else {
-    Sach* sach = new Sach(maSo, ten, chuDe, tacGia, nhaXuatBan, ngayXuatBan, soTrang, soBanLuu);
-    // Xử lý sách không thuộc loại mượn về hoặc mượn đọc theo yêu cầu của bạn
-}
-
-// Thống kê sách mượn về
-cout << "Cac sach cho phep muon ve:" << endl;
-for (const auto& sach : danhSachSachMuonVe) {
-    sach->hienThiThongTin();
-    cout << endl;
-}
-
-// Thống kê sách mượn đọc
-cout << "Cac sach cho phep muon doc:" << endl;
-for (const auto& sach : danhSachSachMuonDoc) {
-    sach->hienThiThongTin();
-    cout << endl;
-}
-
-// Giải phóng bộ nhớ
-for (const auto& sach : danhSachSachMuonVe) {
-    delete sach;
-}
-
-for (const auto& sach : danhSachSachMuonDoc) {
-    delete sach;
+  
+cout << "Danh sach sach cho phep muon doc:" << endl;
+for (const auto& sach : danhSachSach) {
+    SachMuonDoc* sachMuonDoc = dynamic_cast<SachMuonDoc*>(sach);
+    SachMuonVe* sachMuonVe = dynamic_cast<SachMuonVe*>(sach);
+    if (sachMuonDoc != nullptr) {
+        sachMuonDoc->hienThiThongTin();
+        cout << endl;
+    }
 }
 
 
 
+
+ // Giải phóng bộ nhớ cho các đối tượng sách
+    for (Sach* sach : danhSachSach) {
+        delete sach;
+    }
 
     return 0;
 
